c66fac4d91260362e84c9d84651de4b77e521ca9
James-Earley <james.albert.earley@gmail.com>
2022-12-14 10:24:34 +0000

    Create salon.sh

---
+++ salon.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c" 
+
+echo -e "\n~~~~~ Welcome to Alby's Hair Salon ~~~~~~~"
+
+MAIN_MENU() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+
+  echo "Welcome to My Salon, how can I help you?" 
+  echo -e "\n1) cut\n2) color\n3) perm\n4) style\n5) tr\n"
+  read SERVICE_ID_SELECTED
+
+    case $SERVICE_ID_SELECTED in
+      1) SERVICE ;;
+      2) SERVICE ;;
+      3) SERVICE ;;
+      4) SERVICE ;;
+      5) SERVICE ;;
+      *) MAIN_MENU "I could not find that service. What would you like today?" ;;
+    esac
+}
+
+SERVICE () {
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = '$SERVICE_ID_SELECTED' ")
+    #check customer phone number
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+    
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+    
+
+  # if customer doesn't exist
+        if [[ -z $CUSTOMER_NAME ]]
+        then
+          # get new customer name
+          echo -e "\nI don't have a record for that phone number, what's your name?"
+          read CUSTOMER_NAME
+          # insert new customer
+          INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')") 
+          #Request appointment time
+          echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+          #Read new customer details
+          CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+          read SERVICE_TIME
+          INSERT_SERVICE_TIME=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ('$CUSTOMER_ID', '$SERVICE_ID_SELECTED', '$SERVICE_TIME') ")
+          echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+        else
+          # ask for servcie time
+          echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+          read SERVICE_TIME
+          # insert it
+          INSERT_SERVICE_TIME=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ('$CUSTOMER_ID', '$SERVICE_ID_SELECTED', '$SERVICE_TIME') ")
+          echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME." 
+        fi
+  # insert time for haircut
+}
+
+MAIN_MENU
+
+
